6648c9326c040aec70861ba95f0b03a15d20c9bf
sytpb <songyantao@foxmail.com>
2023-05-01 21:39:01 +0800

    debug for response message

--- handler/conversation.js
+++ handler/conversation.js
@@ -4,7 +4,7 @@ import Message from "../comm/message.js";
 import TextChat from "../chat/text.js";*/
 
 export default class Conversation {
-  
+
     constructor() {
     }
 
@@ -13,9 +13,24 @@ export default class Conversation {
     }
 
     async process(body, res) {
-        
-        let chat = null;
-        const data = body;
-        console.log(data);
+        const info = body;
+        const staffID = info?.senderStaffId;
+        const data = {
+            "robotCode": info.robotCode,
+            "userIds": [staffID],
+            "msgKey": "sampleText",
+            "msgParam": JSON.stringify({ "content": "Have got your message" })
+        };
+        const url = 'https://api.dingtalk.com/v1.0/robot/oToMessages/batchSend';
+
+        const config = {
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': "application/json",
+                'x-acs-dingtalk-access-token': '170cd5c344053f008d2e54f5f590e995'
+            }
+        };
+
+        return axios.post(url, data, config);
     }
 }
