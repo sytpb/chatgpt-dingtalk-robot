00b722e651a4dc5e337af0a35c46d63a8537ff8d
sytpb <songyantao@foxmail.com>
2023-05-10 13:36:02 +0800

    debug for variable mode support in yaml,gpt-4

--- service/openai.js
+++ service/openai.js
@@ -3,30 +3,6 @@ import { Configuration, OpenAIApi } from "openai";
 
 const models = ['text-davinci-003','code-davinci-002','gpt-3.5-turbo'];
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-const getAIChat = async (question) => {
-    
-    try {
-        const res = await openai.createChatCompletion({
-            model: models[2],
-            messages:[{role:"user",content: question}]
-        })
-        return res;
-    }
-    catch(error) {
-        console.log("OpenAI happen error!");
-        console.log(error?.response?.data?.error);
-    }
-
-};
-
-export default getAIChat;
-
 
 export class OpenAI {
     #configuration = null;
@@ -44,7 +20,7 @@ export class OpenAI {
 
         try {
             const res = await this.#openai.createChatCompletion({
-                model: models[2],
+                model: process.env.MODE,
                 messages:[{role:"user",content: question}]
             });
 
